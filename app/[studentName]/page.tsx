76a5bca86546705d767d1157078e511384b96847
Ryan James <ryanjames1729@gmail.com>
2024-04-10 12:46:17 -0400

    student task fetching from hygraph working!!!

--- app/[studentName]/page.tsx
+++ app/[studentName]/page.tsx
@@ -5,7 +5,6 @@ import { GraphQLClient } from "graphql-request"
 export async function generateStaticParams() {
     // fetch student names from the graphql endpoint, HYGRAPH_API
 
-    //const HYGRAPH_ENDPOINT = 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clut101wd0wms07wbblx26huy/master'
     const HYGRAPH_ENDPOINT = process.env.HYGRAPH_API
     const graphcms = new GraphQLClient(HYGRAPH_ENDPOINT || '')
 
@@ -24,11 +23,52 @@ export async function generateStaticParams() {
     }))
 }
 
-export default function studentPage({ params }: { params: { studentName: string }}) {
+async function getTasks( studentName: string) {
+    const HYGRAPH_ENDPOINT = process.env.HYGRAPH_API
+    const graphcms = new GraphQLClient(HYGRAPH_ENDPOINT || '')
+    // still working on this query -> variables not working
+    const studentSearch = studentName ? studentName : ""
+    
+    const { toDos }: { toDos: Array<{ id: string, dueDate: string, task: string, taskComplete: boolean, userName: string }>} = await graphcms.request( 
+        `
+        query Assets($studentSearch: String!) {
+            toDos(where: {userName: $studentSearch}) {
+              id
+              dueDate
+              task
+              taskComplete
+              userName
+            }
+          }
+        `, { studentSearch }
+    )
+    
+    return toDos?.map((task) => ({
+        id: task.id,
+        dueDate: task.dueDate,
+        task: task.task,
+        taskComplete: task.taskComplete,
+        userName: task.userName
+    }))
+}
+
+export default async function studentPage({ params }: { params: { studentName: string } }) {
+    const tasks = await getTasks(params.studentName + "@carolinaday.org")
+
     return (
-        <div>
-            STUDENT VIEW
-            {params.studentName}
+        <div className="flex flex-col">
+            <h1>STUDENT VIEW</h1>
+            <h3>Welcome {params.studentName}!</h3>
+
+            {tasks?.map((task) => (
+                <div key={task.id}>
+                    <p>{task.task}</p>
+                    <p>{task.dueDate}</p>
+                    <p>{task.taskComplete}</p>
+                </div>
+            ))}
+
+            {tasks.length > 0 ? '' : <p>Hmm, no tasks found. Are you sure you have an account?</p>}
         </div>
     )
 }
